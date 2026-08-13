90780772a5b3f5bef088649312ad92019f9b4f50
jimenezcirosalome-ai <jimenezcirosalome7.4@gmail.com>
2026-08-13 13:26:29 -0500

    respondemos 4 preguntas de negocio con nuestros datos, usando groupby y gráficos, realizando tambien el reto planteado punto cinco

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import glob 
+import matplotlib.pyplot as plt
 
 #Exploracion de los datos y diferentes tipos de archivos 
 #.csv y xlsx
@@ -122,3 +123,87 @@ print(df_consolidado.head())
 # --------------------------------------------
 df_consolidado.to_excel("consolidado_limpio.xlsx", index=False)
 print("Archivo guardado")
+
+# --------------------------------------------
+# PREGUNTA 1: ¿Cuánto vendió cada categoría en total?
+# (EJEMPLO RESUELTO)
+# --------------------------------------------
+ventas_categoria = df_consolidado.groupby('categoria')['precio_unitario'].sum()
+print(ventas_categoria)
+
+ventas_categoria.plot(kind='bar', title='Ventas por Categoria')
+plt.ticklabel_format(style='plain', axis='y')
+plt.ylabel('Ventas totales ($)')
+plt.xlabel('Categoría')
+plt.xticks(rotation=0)
+plt.tight_layout()
+plt.savefig("grafico_categoria.png")
+plt.show()
+
+# --------------------------------------------
+# PREGUNTA 2: ¿Qué porcentaje de las ventas representa 
+# cada vendedor?
+# --------------------------------------------
+ventas_vendedor = df_consolidado.groupby('vendedor')['precio_unitario'].sum()
+print(ventas_vendedor)
+ventas_vendedor.plot(
+    kind='pie',
+    autopct='%1.1f%%',
+    title='Porcentaje de Ventas por Vendedor'
+)
+
+plt.ylabel('')
+plt.tight_layout()
+
+plt.savefig("grafico_vendedor.png")
+
+plt.show()
+
+# --------------------------------------------
+# PREGUNTA 3: ¿Cuál es el producto que más se vende?
+# --------------------------------------------
+productos_vendidos = df_consolidado['producto'].value_counts()
+print(productos_vendidos)
+# Paso 3: impriman el resultado
+
+# --------------------------------------------
+# PREGUNTA 4: ¿Cómo se distribuyen las ventas según 
+# el método de pago?
+# --------------------------------------------
+metodo_pago = df_consolidado.groupby('metodo_pago')['precio_unitario'].sum()
+print(metodo_pago)
+metodo_pago.plot(
+    kind='pie',
+    autopct='%1.1f%%',
+    title='Distribución de Ventas por Método de Pago'
+)
+
+plt.ylabel('')
+plt.tight_layout()
+
+plt.savefig("grafico_metodo_pago.png")
+
+plt.show()
+
+
+# --------------------------------------------
+# PREGUNTA 5: ¿Cuál es el día de la semana con más ventas?
+# --------------------------------------------
+
+df_consolidado["fecha"] = pd.to_datetime(
+    df_consolidado["fecha"],
+    errors="coerce"
+)
+
+df_consolidado["dia_semana"] = df_consolidado["fecha"].dt.day_name()
+
+ventas_dia = df_consolidado.groupby("dia_semana")["precio_unitario"].sum()
+
+print("--- VENTAS POR DÍA DE LA SEMANA ---")
+print(ventas_dia)
+
+dia_mayor_venta = ventas_dia.idxmax()
+mayor_venta = ventas_dia.max()
+
+print(f"El día de la semana con más ventas es: {dia_mayor_venta}")
+print(f"Total de ventas: ${mayor_venta:,.0f}")
